42c8d1b95f1ba3f65a275d42bee5f38fcb872856
veromejia <veromejia.q@gmail.com>
2019-09-19 21:57:50 -0700

    last-UPDATE

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+
+    for key, value in a_dictionary.items():
+        if value is max(a_dictionary.values()):
+            return key
