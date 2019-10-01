b1e28a54a136ade0ba715db4f20552b2da97ef6d
veromejia <veromejia.q@gmail.com>
2019-09-30 17:57:29 -0700

    changing stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as e:
-        print("Exception:", sys.exc_info()[1])
+    except ValueError as val_error:
+        print("Exception: {}".format(val_error), file=sys.stderr)
         return False
