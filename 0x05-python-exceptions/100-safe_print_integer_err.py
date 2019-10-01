a7a9c95953e5630bc12569cfae9e5be7eb9ad68a
veromejia <veromejia.q@gmail.com>
2019-09-30 17:59:54 -0700

    exception error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as val_error:
+    except Exception as val_error:
         print("Exception: {}".format(val_error), file=sys.stderr)
         return False
