acc53080a523271e6fa17a1b4a5b78f7009bc7b8
veromejia <veromejia.q@gmail.com>
2019-09-30 17:20:27 -0700

    system error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        print("Exception:", sys.exc_info()[1])
+        return True
+    except (ValueError, TypeError):
+        print("Exception:",sys.exc_info()[1])
         return False
-    return True
