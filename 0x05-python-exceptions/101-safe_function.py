8ac16413d3b3fbeb8c3a61f83da76e06082ba26d
veromejia <veromejia.q@gmail.com>
2019-09-30 18:33:04 -0700

    pep8 fixed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return result
     except Exception as fct_error:
-        print("Exception: {}", format(fct_error), file=sys.stderr)
+        print("Exception: {}".format(fct_error), file=sys.stderr)
         return None
