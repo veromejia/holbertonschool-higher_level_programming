874086f992aebe3fffbd15e72ec27063d1bf4642
veromejia <veromejia.q@gmail.com>
2019-10-01 17:47:25 -0700

    fixed pep8 error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,11 +2,11 @@
 class Square:
     ''' Square class '''
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         ''' Inicializing size field'''
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
