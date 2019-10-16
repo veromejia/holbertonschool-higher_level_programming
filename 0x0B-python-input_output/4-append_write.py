ca4ac1fa576ed4a91350bde536d8d19ff52b8e8b
veromejia <veromejia.q@gmail.com>
2019-10-16 15:16:41 -0700

    tasks 2-4

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to appends a string at the end of a file"""
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file (UTF8)
+    and returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
