8add77c6aade0f4188872749e179bf1e95e8c51e
veromejia <veromejia.q@gmail.com>
2019-11-25 17:11:10 -0800

    6-8

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- reates a table called first_table in the current db in your MySQL server.
+-- If the table first_table already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table first_table of the db  hbtn_0c_0
+-- displays the numer of records with id =89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
