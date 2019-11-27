6f487cba5bce12df4752cae0dd41565ff6d3aa92
veromejia <veromejia.q@gmail.com>
2019-11-26 16:49:44 -0800

    change option to use db

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa
 -- creates database hbtn_od_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- creates table cities into database hbtn_od_usa
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+CREATE TABLE IF NOT EXISTS cities(
 id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
