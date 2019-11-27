ed88ce8e52b5d7f1f7fd2ff9f30dde0b80258d80
veromejia <veromejia.q@gmail.com>
2019-11-26 17:20:43 -0800

    fixed comment

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
---sts all the cities of California that can be found in the db hbtn_0d_usa.
+-- lists all the cities of California that can be found in the db hbtn_0d_usa.
 -- lists all the cities of California
   SELECT id, name FROM cities WHERE state_id = (
   SELECT id FROM states WHERE name = "California")
