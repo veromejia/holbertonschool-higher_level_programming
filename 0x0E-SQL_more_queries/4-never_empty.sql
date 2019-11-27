6a6c53f6df17711792f37499d491095e76a8eb7e
veromejia <veromejia.q@gmail.com>
2019-11-26 16:07:37 -0800

    id not null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- creates the table id_not_null on your MySQL server.
+-- creates the table id_not_null with correstonding attributes
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
