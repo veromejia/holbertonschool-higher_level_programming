f84241d5442de17e5b66c2ecbf9d1a2bde4e41c1
veromejia <veromejia.q@gmail.com>
2019-12-14 23:28:57 -0800

    fixed sensitive case

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                            db="{}".format(sys.argv[3]),
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4]) +
-                "ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+    ORDER BY states.id ASC".format(sys.argv[4]))
 
     query_rows = cur.fetchall()
     for row in query_rows:
