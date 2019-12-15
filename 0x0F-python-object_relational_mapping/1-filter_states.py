49511b6e914550f2c5a78e79b2e81487e73814a4
veromejia <veromejia.q@gmail.com>
2019-12-14 22:26:59 -0800

    fixed sentive case

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
                            db="{}".format(sys.argv[3]),
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'" +
+                "ORDER BY id ASC")
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
