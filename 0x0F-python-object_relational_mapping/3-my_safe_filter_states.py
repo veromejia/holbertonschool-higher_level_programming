f18a714eeff8442b2c1a7cc8a3a2a48adac28bc8
veromejia <veromejia.q@gmail.com>
2019-12-15 00:54:27 -0800

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument. """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="{}".format(sys.argv[1]),
+                           passwd="{}".format(sys.argv[2]),
+                           db="{}".format(sys.argv[3]),
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name=%s", [sys.argv[4]])
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
