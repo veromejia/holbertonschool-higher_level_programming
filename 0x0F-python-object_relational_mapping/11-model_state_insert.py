410855e7843d3787dd358da1c3f9ae962da9b38f
veromejia <veromejia.q@gmail.com>
2019-12-15 18:58:10 -0800

    fixed spelling

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = State(name='Lousiana')
+
+    states = State(name='Louisiana')
 
     session.add(states)
     session.commit()
